189e6e710ded502d0ae481bd4392632f888d9213
ivieomofoma <ivie.omofoma@gmail.com>
2023-02-21 13:07:23 -0800

    Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -2,14 +2,15 @@
 
 /**
  * main - check the code
- *
+ *declaration
+
  * Return: Always 0.
  */
 int main(void)
 {
     int r;
 
-    print_last_digit(98);
+    print_last_digit();
     print_last_digit(0);
     r = print_last_digit(-1024);
     _putchar('0' + r);
